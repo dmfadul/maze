e9446bb902b969f8bd739f5b74795be03c5d2f01
home <dmf030@gmail.com>
2023-09-14 13:11:22 -0300

    first commit

---
+++ nn.py
@@ -0,0 +1,20 @@
+# import numpy as np
+# import tensorflow as tf
+#
+#
+# rand_a = np.random.uniform(0, 100, (5, 5))
+# # print(rand_a)
+#
+# rand_b = np.random.uniform(0, 100, (5, 1))
+# print(rand_b)
+#
+# a = tf.
+# a = tf.placeholder(tf.float32)
+# b = tf.placeholder(tf.float32)
+#
+# add_op = a + b
+# mul_op = a * b
+#
+# with tf.Session() as sess:
+#     add_result = sess.run(add_op, feed_dict={a: 10, b: 20})
+#     print(add_result)
